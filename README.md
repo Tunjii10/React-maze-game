e43fb33b444aee814460b550e4bbee387f34b2f1
Tunjii10 <oluwatunji7@gmail.com>
2022-04-21 18:36:40 +0100

    game-desgin

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # React-maze-game
 A maze game built with React JS
+
+**This is a submission for the hackerbay interview frontend task.**
+
+## Setup
+
+**1.** Clone the repo.
+
+```
+git clone https://github.com/Tunjii10/React-maze-game.git
+```
+
+**2.** `cd` into the repo and install dependencies with
+
+```
+npm install
+```
+
+**3.** The app runs on port 3000 with `npm start`.
+
+## How to Play
+First input board width(number) and height(number).
+Control the mario character with the up/down/left/right arrow key. Game is over when moves are up to 64. 
+Once all sprites are eaten without exceeding 64 moves, game is won.
+
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,10 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import sprite from "../images/mushroom.png"
+import mario from "../images/SeekPng.com_video-game-character-png_942386.png"
 
 const Game = () => {
     const [gameArray, setGameArray] = useState(null)
+    const gameArrayRef = useRef(null)
+    const mario_position = useRef(null)
+    const move_count = useRef(0)
+    const sprites_remaining = useRef(0) 
+
     const create_game_array = (width, height) => {
         let arr =  Array(height).fill(0).map(() => Array(width).fill(0))
         let mid = [Math.floor(arr.length/2), Math.floor(arr[0].length/2)]
+        mario_position.current = [...mid, width, height]
         arr[mid[0]][mid[1]] = 1 
         let occupiedSpots = 0;
         while(occupiedSpots < (width > height ? height : width)){
@@ -15,15 +23,115 @@ const Game = () => {
                 occupiedSpots++;
             }
         }
+        sprites_remaining.current = occupiedSpots
+        gameArrayRef.current = arr
         setGameArray(arr)
     }
 
+    const displayCharacter = (x) => {
+        if (x == 2) {
+            return (
+                <div className="sprite">
+                    <img  src={sprite}/>
+                </div>
+                )
+        } else if ( x == 1) {
+            return (
+                <div className="mario">
+                    <img  src={mario}/>
+                </div>
+            )
+        } else {
+            return (
+                <div></div>
+            )
+        }
+    }
+    const endGame = () => {
+        if (sprites_remaining.current === 0){
+            alert("Game Won")
+            window.location.reload(false);
+        }
+        if (move_count.current === 65){
+            alert("Game Over : Total moves to save princess: 64")
+            window.location.reload(false);
+        }
+       
+    }
+
+    const handleKeyPress = (e) => {
+        let gameArray_copy = gameArrayRef.current
+        let [check_height, check_width, max_width, max_height] = mario_position.current
+        if (e.keyCode === 38){
+           // key press up 
+           if (check_height-1 >= 0){
+                if (gameArray_copy[check_height-1][check_width] == 2){
+                    sprites_remaining.current -= 1
+                }
+                gameArray_copy[check_height][check_width] = 0
+                gameArray_copy[check_height-1][check_width] = 1
+                mario_position.current = [check_height-1, check_width, max_width, max_height]
+                gameArrayRef.current = gameArray_copy
+                move_count.current += 1
+                setGameArray([...gameArray_copy])
+            }
+        }
+        if (e.keyCode === 40){
+            // key press down 
+            if (check_height+1 < max_height){
+                if (gameArray_copy[check_height+1][check_width] == 2){
+                    sprites_remaining.current -= 1
+                }
+                gameArray_copy[check_height][check_width] = 0
+                gameArray_copy[check_height+1][check_width] = 1
+                mario_position.current = [check_height+1, check_width, max_width, max_height]
+                gameArrayRef.current = gameArray_copy
+                move_count.current += 1
+                setGameArray([...gameArray_copy])
+               }
+        }
+        if (e.keyCode === 37){
+            // key press left 
+            if (check_width-1 >= 0){
+                if (gameArray_copy[check_height][check_width-1] == 2){
+                    sprites_remaining.current -= 1
+                }
+                gameArray_copy[check_height][check_width] = 0
+                gameArray_copy[check_height][check_width-1] = 1
+                mario_position.current = [check_height, check_width-1, max_width, max_height]
+                gameArrayRef.current = gameArray_copy
+                move_count.current += 1
+                setGameArray([...gameArray_copy])
+               }
+        }
+        if (e.keyCode === 39){
+            // key press right 
+            if (check_width+1 < max_width){
+                if (gameArray_copy[check_height][check_width+1] == 2){
+                    sprites_remaining.current -= 1
+                }
+                gameArray_copy[check_height][check_width] = 0
+                gameArray_copy[check_height][check_width+1] = 1
+                mario_position.current = [check_height, check_width+1, max_width, max_height]
+                gameArrayRef.current = gameArray_copy
+                move_count.current += 1
+                setGameArray([...gameArray_copy])
+               }
+        }
+    }
+
     useEffect(() => {
-        const width = prompt("what is the board width")
-        const height = prompt("what is the board height")
+        const width = prompt("Please enter board width")
+        const height = prompt("Please enter board height")
         create_game_array(+width, +height)
     },[])
-    console.log(gameArray)
+    
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyPress)
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress)
+        }
+    }, [])
     
     return (
         gameArray && 
@@ -32,18 +140,18 @@ const Game = () => {
                 <tbody>
                     {
                         gameArray.map((x,y) => (
-                            <tr key = {y+"row"}>
+                            <tr key = {y+"row"} >
                                 {x.map((a,b) => (
-                                    <td key={y+b+"col"}>
-                                        {a}
+                                    <td key={y+b+"col"} id="row">
+                                        {displayCharacter(a)}
                                     </td>
                                 ))}
                             </tr>
                         ))
                     }
                 </tbody>
+                {endGame()}
             </table>
-            <p>hello</p>
         </div>
     )
 }
